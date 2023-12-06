184c02c786c0a91bc25eea92b2031458c3243b14
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-06 11:37:02 -0500

    CRUD operations with SQLALchemy

--- main.py
+++ main.py
@@ -34,6 +34,42 @@ class Books(db.Model):
 
 with app.app_context():
     db.create_all()
-    book = Books(title="Harry Potter", author="J. K. Rowling", review=9.3)
-    db.session.add(book)
+    # CRUD
+    # CREATE A NEW RECORD
+    book1 = Books(title="Harry Potter", author="J. K. Rowling", review=9.3)
+    book2 = Books(title="The Black Panther", author="Charles Dickens", review=7.5)
+    book3 = Books(title="Goku", author="Christian Tsoungui Nkoulou", review=7.5)
+    db.session.add(book1)
+    db.session.add(book2)
+    db.session.add(book3)
     db.session.commit()
+
+    # READ ALL RECORDS
+    # create a "query" to select things from the database and order by title
+    result = db.session.execute(db.select(Books).order_by(Books.title))
+    all_books = result.scalars().all()
+    for b in all_books:
+        print(f"title: {b.title} author: {b.author} review: {b.review}")
+
+    # READ A PARTICULAR RECORD BY QUERY
+    book = db.session.execute(db.select(Books).where(Books.title == "Harry Potter")).scalar()
+    print(book.title)
+
+    # UPDATE A PARTICULAR RECORD BY QUERY
+    book_to_update = db.session.execute(db.select(Books).where(Books.title == "The Black Panther")).scalar()
+    book_to_update.title = "Black Panther"
+    db.session.commit()
+
+    # UPDATE A RECORD BY PRIMARY KEY
+    book_id = 2
+    book_to_update = db.session.execute(db.select(Books).where(Books.id == book_id)).scalar()
+    book_to_update.title = "Avatar The Last Air Bender"
+    db.session.commit()
+
+    # DELETE A PARTICULAR RECORD BY PRIMARY KEY
+    book_id = 2
+    book_to_delete = db.session.execute(db.select(Books).where(Books.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+
+
