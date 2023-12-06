b135ebce9e49964af81fc5c899c4daba0afad517
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-06 10:01:16 -0500

    Configure extension

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
+
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
 
 # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) "
 #                "NOT NULL, rating FLOAT NOT NULL)")
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J.k. Rowling', '9.3')")
-db.commit()
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J.k. Rowling', '9.3')")
+# db.commit()
+
+db = SQLAlchemy()
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+
+db.init_app(app)
+
+
 
